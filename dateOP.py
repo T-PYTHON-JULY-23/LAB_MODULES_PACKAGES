628072cc61b18fed20dfe06310513e209f1fee13
MaramFahad <maramll98ll@gmail.com>
2023-08-02 13:43:53 +0300

    lab added

---
+++ dateOP.py
@@ -0,0 +1,6 @@
+from datetime import date
+#import datetime
+def today_date():
+    print(date.today())
+
+today_date()
---
+++ main.py
@@ -0,0 +1,6 @@
+#import dateOP 
+from dateOP import today_date
+
+print("current date is: ")
+current_date=today_date()
+
