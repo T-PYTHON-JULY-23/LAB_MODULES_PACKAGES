57d24f0e3ffccc4e7e2a7bf3c22027f11a0bd01a
Lama Alsharif <lama.a.alsharif@gmail.com>
2023-08-02 13:47:36 +0300

    LAB_MODULES_PACKAGES

---
+++ dateOP.py
@@ -0,0 +1,5 @@
+from datetime import date # >>>> import the date class from the datetime module
+def current_date (): # >>>> function that when called prints the current date.
+    currentDate= date.today()
+    print(f"the date today is : {currentDate}  ")
+
---
+++ main.py
@@ -0,0 +1,3 @@
+import dateOP # >>>> import dateOP module
+
+dateOP.current_date() # >>> call the function that print the current date
