31439c231f43a3d30fda2046849bf2c028efd67f
xluuu <mlikmozini@gmail.com>
2023-08-03 01:27:30 +0300

    Modules and packages

--- main.py
+++ main.py
@@ -2,15 +2,8 @@ import dateOP
 dateOP.current_date()
 from library import librarian
 library = {}
-
-#add book
 librarian.add_book(library, "Rish dad and poor dad", "Abdulmalik", "12345")
 librarian.add_book(library, "Harry Potter", "Chrisitina", "32423")
-#check out book
 librarian.check_out_book(library, "12345")
-
-#return book
 librarian.return_book(library, "12345")
-
-
 librarian.display_books(library)
