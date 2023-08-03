a4c26563b2a3e88732a605d8dcb5653552e46f38
Nada Almutairi <nadaamusaad@gmail.com>
2023-08-03 11:37:46 +0300

    done

--- Library/librarian.py
+++ Library/librarian.py
@@ -1,9 +1,10 @@
 def add_book(library:dict, title:str, author:str, isbn:str):
     if isbn in library:
+        
+        print( "the book in  dictinary")
+    else:
         book= {"title" : title, "author":author ,"available":True }
         library[isbn] = book 
-        print( "the book is available")
-    else:
         print ( "The book is added")
         
 def remove_book(library:dict, isbn:str):
@@ -16,21 +17,26 @@ def remove_book(library:dict, isbn:str):
 def check_out_book(library:dict , isbn:str):
     book= library.get(isbn)
 
-    if  book["avalbile"]==True:
-       book["avalbile"] =False
-       print("done check out")
+    if  book["available"]==True:
+        book["available"] =False
+        print("done check out")
     else:
        print(" book is already check out ")
        
 def return_book (library:dict , isbn:str):
-     if book["avalbile"]==True:
+     book= library.get(isbn)
+     if book["available"]==False:
+         book["available"] =True
          print ("the book is returned")
      else:
          print("not exist")
     
-def display_books (library:dict , isbn:str):
-    if book in library.values():
-      exist = book['available']
-    else: "checked out"
-    print(f"{book['title']} by {book['author']} ({book['isbn']}) - {exist}") 
+def display_books (library:dict):
+    for isbn, book in library.items():
+     if book['available']==True:
+        exist="available"
+    
+     else: 
+      exist= "checked out"
+     print(f"{book['title']} by {book['author']} ({isbn}) - {exist}") 
     
--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from Library import librarian
 
 print_current_date()
 library={}
-librarian.add_book(library ,"The Catcher in the Rye","J.D. Salinger","9780316769174")
-librarian.add_book(library,"To Kill a Mockingbird","Harper Lee","9780446310789")
-librarian.add_book(library,"1984","George Orwell","9780451524935")
-librarian.check_out_book(library,"The Catcher in the Rye" , "J.D. Salinger" , "9780316769174")
-librarian.display_books()
+librarian.add_book(library,"The Catcher in the Rye" , "J.D. Salinger" , "9780316769174")
+librarian.add_book(library, " To Kill a Mockingbird","Harper Lee","9780446310789")
+librarian.add_book(library ,"1948", "George Orwell","9780451524935" )
+librarian.display_books(library)
+
+librarian.check_out_book (library, "9780316769174")
+librarian.display_books(library)
+
+librarian.return_book (library, "9780316769174")
+librarian.display_books(library)
